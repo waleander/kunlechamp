f56230e5184b16543c4df7153413f50947720747
Akin Oluyele <waleander@gmail.com>
2017-08-15 20:17:27 +0100

    add a server for the index; separate handler in a directory: separate routes in a file

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({port: 3000, host: 'localhost'});
+server.bind({
+  apiBaseUrl: 'http://localhost:4000/api',
+  WebBaseUrl: 'http://localhost:4000'
+});
+
+
+
+
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route(require('./routes.js'));
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
