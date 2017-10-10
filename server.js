5fa85a0afa62501ecd9ae1c7c73f9750f79a727b
Akin Oluyele <waleander@gmail.com>
2017-10-10 16:31:31 +0100

    added google map, upcoming events to the Home page, javascript text writing functionality

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
-'use strict';
-
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 server.connection({
-    port: parseInt(process.env.PORT, 10) || 3000,
-    host: '0.0.0.0'
+  port: parseInt(process.env.PORT, 10) || 3000,
+  host: '0.0.0.0',
 });
 server.bind({
   apiBaseUrl: 'http://localhost:4000/api',
   WebBaseUrl: 'http://localhost:4000'
 });
 
-
-
-
 server.register(require('inert'), (err) => {
+  if (err) {
+    throw err;
+  }
 
-    if (err) {
-        throw err;
-    }
-
-    server.route(require('./routes.js'));
+  server.route(require('./routes.js'));
 });
 
 server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
 });
